7c0eb5c5559183d785b9ff0f91b6cc005cbdc71f
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2024-09-13 21:11:59 +0500

    operators

---
+++ dart/operator.dart
@@ -0,0 +1,34 @@
+// it is kinda same this js
+// + - add
+// - - subtract
+// * - multiply
+// / - divide
+// ~/ - yaxlitlab divide
+// % - qoldiq
+
+// there are two this a++ and ++a
+
+// int num = 0
+// int a = num++
+// print(a) // is equil to 0, but if we print num it is 1
+// int b = ++num
+// print(b) //is equil to 1, but if we print num it is 0
+
+
+// ?? if that this is null then make it equil to this
+// b ??='Roziya' if b = null then make it Roziya but if it not don't change anything  
+
+
+
+// ...
+// var paint = Paint()
+//   ..color = Colors.black
+//   ..strokeCap = StrokeCap.round
+//   ..strokeWidth = 5.0;
+
+
+// this is equil to 
+// var paint = Paint();
+// paint.color = Colors.black;
+// paint.strokeCap = StrokeCap.round;
+// paint.strokeWidth = 5.0;
