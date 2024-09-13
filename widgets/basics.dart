4014b9656b4e7e4576f5c9ee2e161d89e732160b
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2024-09-13 21:40:33 +0500

    basic widgets (not full)

---
+++ widgets/basics.dart
@@ -0,0 +1,11 @@
+// Text - just to write string
+// Row, Column - Horizantal(Row) Verticale(Column)
+// Stack - it is both of row and column we can choose it of its paramentr Positions
+// Container-it is just rectangle element box, there is boxContainer which have background, shadow, border
+
+
+// Material widget which help to build app that follow Material Design  
+
+// GestureDetector -- this widgets don't effect to UI 
+// if you have btn and every click you want to get info in console this widget will help you at that point
+// it has onTap() callback which give info every time when click that widget UI part 
