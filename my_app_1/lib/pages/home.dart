980c5d9cef91c40589e6726822b862a80ebaf7d9
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2024-09-17 21:58:27 +0500

    tried to add background image but failed:(

--- my_app_1/lib/pages/home.dart
+++ my_app_1/lib/pages/home.dart
@@ -5,6 +5,41 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          child: Text('Hi'),
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/mainback.png"),
+              fit: BoxFit.cover,
+            ),
+            color: Color(0xfff7f8f8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+      body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/mainBack.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Text('Hi')),
+    );
   }
 }
