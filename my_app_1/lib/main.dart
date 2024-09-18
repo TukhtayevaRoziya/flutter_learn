a66751426f9528a905d4fba01c1ec3aeefd323e8
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2024-09-18 22:07:36 +0500

    background image done, and move containers to end of the screen done

--- my_app_1/lib/main.dart
+++ my_app_1/lib/main.dart
@@ -11,16 +11,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: 'Poppins'),
-      debugShowCheckedModeBanner: false,
-      home: Container(
+        theme: ThemeData(fontFamily: 'Poppins'),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
               image: AssetImage("assets/mainBack.png"),
               fit: BoxFit.cover,
             ),
           ),
-          child: Text('Hi')),
-    );
+          child: Container(
+            child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+              Text(
+                '23 34Coffee so good, your taste buds will love it.',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 34,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                  child: Row(
+                children: [
+                  Image.asset('assets/google_icon.png'),
+                  Text('sdfsdf')
+                ],
+              )),
+              Text(
+                '45 23 34Coffee so good, your taste buds will love it.',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 34,
+                    fontWeight: FontWeight.bold),
+              ),
+            ]),
+          ),
+        )));
   }
 }
