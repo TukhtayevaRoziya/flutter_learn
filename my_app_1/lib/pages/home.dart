2bd159b15c524bd96154cf775b694197c9e97efd
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2024-09-23 20:28:43 +0500

    home page search and header part done ------ But ALready fed up from FLutter:) :(

--- my_app_1/lib/pages/home.dart
+++ my_app_1/lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  
   final String data;
 
   HomePage({required this.data});
@@ -9,19 +10,74 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(data),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text('Back to First Screen'),
-          ),
-        ],
-      ),
+      child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+        Container(
+            color: Color(0xff242424),
+            padding: EdgeInsets.only(left: 30, right: 30),
+            height: 270,
+            width: MediaQuery.of(context).size.width * 1,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Row(children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Location',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      'Bilzen,Tanjungbalai',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    ),
+
+                    // ),
+                  ],
+                ),
+                Expanded(
+                    child: Image.asset(
+                  'assets/profilePic.png',
+                  fit: BoxFit.contain,
+                  alignment: Alignment.bottomCenter,
+                  width: 70,
+                  height: 60,
+                ))
+              ]),
+              Column(
+                children: [
+                  TextFormField(
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: const InputDecoration(
+                        //  border: InputBorder(borderSide: BorderSide(width: 15, C)),
+                        hintText: 'Search coffee',
+                        fillColor: Colors.white),
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter Search coffee';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+            ])),
+        Expanded(
+            child: Image.asset(
+          'assets/home_1.png',
+          fit: BoxFit.contain,
+          alignment: Alignment.bottomCenter,
+          width: 70,
+          height: 60,
+        )),
+        Text('sdfsd')
+      ]),
     ));
   }
 }
