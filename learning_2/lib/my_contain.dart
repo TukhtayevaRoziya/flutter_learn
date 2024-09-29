57002088db77117061fafae05e71fe8986be16c2
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2024-09-29 19:08:02 +0500

    contain learning done ✔

--- learning_2/lib/my_contain.dart
+++ learning_2/lib/my_contain.dart
@@ -1,3 +1,5 @@
+// import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class MyContain extends StatelessWidget {
@@ -19,16 +21,40 @@ class MyContain extends StatelessWidget {
           child: Container(
             width: 200,
             height: 200,
+            transform: Matrix4.skewY(100),
             decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(
+              color: Colors.blue,
+              // borderRadius: BorderRadius.circular(12),
+              border: Border.all(
+                  color: Colors.red,
+                  strokeAlign: BorderSide.strokeAlignInside,
+                  width: 13),
+              // image: DecorationImage(
+              //     image: NetworkImage(
+              //         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXJA32WU4rBpx7maglqeEtt3ot1tPIRWptxA&s')),
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 30,
                     color: Colors.red,
-                    strokeAlign: BorderSide.strokeAlignInside,
-                    width: 13),
-                image: DecorationImage(
-                    image: NetworkImage(
-                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXJA32WU4rBpx7maglqeEtt3ot1tPIRWptxA&s'))),
+                    // spread where dx, dy
+                    offset: Offset(0, 0),
+                    // boxni nechi sm dan keyin box shadow ishlasin
+                    // spreadRadius: 1,
+                    // spread which inner outer
+                    blurStyle: BlurStyle.outer)
+              ],
+              gradient: LinearGradient(
+                  // begin: Alignment.topRight,
+                  // end: Alignment.bottomLeft,
+                  colors: [
+                    Colors.orange,
+                    Colors.black,
+                    Colors.orange,
+                    Colors.black,
+                    Colors.orange,
+                  ]),
+            ),
           ),
         ));
   }
